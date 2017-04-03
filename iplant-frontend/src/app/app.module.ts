91bddbf932bb5dd7dd96aa7bccc0440f7bb9fd75
Juan David Insuasti Bravo <juan.insuasti@gmail.com>
2017-04-03 15:27:21 -0500

    Component Creation

--- iplant-frontend/src/app/app.module.ts
+++ iplant-frontend/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DeviceComponent } from './device/device.component';
+import { ListComponent } from './shared/list/list.component';
+import { DetailsComponent } from './shared/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DeviceComponent,
+    ListComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
